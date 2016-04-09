6b28992e2c8f927497858d84bd0007dff0b6dcc4
Steven Ma <machong@umich.edu>
2016-04-09 14:02:25 -0400

    updated main

--- main.cpp
+++ main.cpp
@@ -24,6 +24,8 @@ int main(int argc, const char * argv[]) {
     sudoku = adjSize(sudoku, min_area);
     
     sudoku = straighten(sudoku);
+    
+    imshow("straight", sudoku);
 
     Mat cells, nums;
     findCell(sudoku, cells, nums);
@@ -60,87 +62,3 @@ int main(int argc, const char * argv[]) {
     cvWaitKey();
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//int main(int argc, const char * argv[]) {
-//    const double std_area = 160000.0;
-//    
-//    //Load image
-//    Mat sudoku = imread("/Users/stevenma/Desktop/EECS 442/testCV2/testCV2/sudoku.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-//    
-//    double im_area =  sudoku.size().height * sudoku.size().width;
-//    
-//    double sz_ratio = sqrt(std_area/im_area);
-//    
-//    Size new_sz(sudoku.size().height*sz_ratio, sudoku.size().width*sz_ratio);
-//    
-//    resize(sudoku, sudoku, new_sz);
-//    
-//    sudoku = straighten(sudoku);
-//    Mat sudoku_blur;
-//    
-//    Mat grid = Mat(sudoku.size(), CV_8UC1);
-//    Mat num = Mat(sudoku.size(), CV_8UC1);
-//    GaussianBlur(sudoku, sudoku_blur, Size(11,11), 0);
-//    
-//    imshow("blur", sudoku);
-//    
-//    imshow("balance", sudoku);
-//    //threshold(sudoku, sudoku, 230, 255, THRESH_BINARY);
-//    imshow("shrp", sudoku);
-//    
-//    adaptiveThreshold(sudoku_blur, num, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 2);
-//    adaptiveThreshold(sudoku, grid, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 15, 2);
-//    
-//    bitwise_not(grid, grid);
-//    Mat kernel1 = (Mat_<uchar>(3,3) << 0,1,0,1,1,1,0,1,0);
-//    Mat kernel2 = (Mat_<uchar>(5,5) << 0,0,1,0,0, 0,1,1,1,0, 1,1,1,1,1, 0,1,1,1,0, 0,0,1,0,0);
-//    //below is code for detecting the outer box, all items are colored 64 in the process
-//    Point maxPt = shLargestFlood(grid);
-//    
-//    //color the largest area white
-//    floodFill(grid, Point(0,0), CV_RGB(255,255,255));
-//    floodFill(grid, maxPt, CV_RGB(255,255,255));
-//    floodFill(num, maxPt, CV_RGB(255,255,255));
-//    //this loop goes over the image and colors all the grey areas black
-//    shGetRidOfColor(grid, 64);
-//    shGetRidOfColor(num, 64);
-//    
-//    dilate(grid, grid, kernel2);
-//    
-//    GaussianBlur(grid, grid, Size(7,7), 0);
-//    threshold(grid, grid, 50, 255, THRESH_BINARY);
-//    
-//    bitwise_or(num, grid, num);
-//    
-//    bitwise_not(num, num);
-//    dilate(num, num, kernel1);
-//    
-//    GaussianBlur(num, num, Size(5,5), 0);
-//    bitwise_not(num, num);
-//    adaptiveThreshold(num, num, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 2);
-//    
-//    
-//    
-//    imshow("num3", num);
-//    imshow("outerBox3:",grid);
-//    cvWaitKey();
-//    return 0;
-//}
-
